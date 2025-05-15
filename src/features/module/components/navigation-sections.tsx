5d51628a47a1de1a7c302861cdf4b1bb1047b8ff
Rolucatelli <rolucatelli@gmail.com>
2025-05-15 11:26:08 -0300

    feat(module): finished  introduction footer

--- src/features/module/components/navigation-sections.tsx
+++ src/features/module/components/navigation-sections.tsx
@@ -3,22 +3,38 @@ import { SectionButton } from './section-button';
 const api = {
   // topics: [{ title: 'Simple Present' }, { title: 'Simple Past' }],
   topics: [
-    { title: 'Simple Present' },
-    { title: 'Simple Past' },
-    { title: 'Past Perfect' },
+    {
+      title: 'Simple Present',
+      isModified: true,
+    },
+    {
+      title: 'Simple Past',
+      isModified: false,
+    },
+    {
+      title: 'Past Perfect',
+      isModified: true,
+    },
   ],
 };
 
 export const NavigationSections = () => {
   return (
-    <footer className="fixed bottom-0 left-0 w-full">
-      <div className="bg-primary overflow-x-auto whitespace-nowrap">
-        <SectionButton variant="introduction" isCompleted />
+    <footer className="bg-primary fixed bottom-0 left-0 w-full overflow-x-auto whitespace-nowrap">
+      <div className="md:flex md:justify-center">
+        <SectionButton variant="introduction" title="Introdução" isStarted />
         {api.topics.map((topic) => {
-          return <SectionButton key={topic.title} variant="topic" />;
+          return (
+            <SectionButton
+              key={topic.title}
+              variant="topic"
+              title={topic.title}
+              isStarted={topic.isModified}
+            />
+          );
         })}
-        <SectionButton variant="finalChallenge" />
-        <SectionButton variant="revision" />
+        <SectionButton variant="finalChallenge" title="Desafio Final" />
+        <SectionButton variant="revision" title="Revisão" />
       </div>
     </footer>
   );
--- src/features/module/components/section-button.tsx
+++ src/features/module/components/section-button.tsx
@@ -3,15 +3,28 @@ import { SECTIONS_CREATION_MODULE } from '@/constants/module';
 
 interface SectionButton {
   variant: 'introduction' | 'topic' | 'finalChallenge' | 'revision';
-  isCompleted?: boolean;
+  isStarted?: boolean;
+  title: string;
 }
 
-export const SectionButton = ({ variant, isCompleted }: SectionButton) => {
+export const SectionButton = ({ variant, isStarted, title }: SectionButton) => {
   const Icon = SECTIONS_CREATION_MODULE[variant].icon;
 
+  let btnClasses =
+    'my-6 ml-6 bg-zinc-50 py-6 focus:bg-zinc-50 hover:bg-zinc-50';
+
+  if (variant === 'revision') {
+    btnClasses += ' mr-6';
+  }
+
+  if (!isStarted) {
+    btnClasses += ' md:opacity-50';
+  }
+
   return (
-    <Button className="mx-auto my-6 bg-zinc-50 py-6">
-      <Icon className="text-primary size-8" opacity={isCompleted ? 100 : 50} />
+    <Button className={btnClasses}>
+      <Icon className="text-primary size-8" opacity={isStarted ? 1 : 0.5} />
+      <h1 className="text-primary not-md:hidden">{title}</h1>
     </Button>
   );
 };
