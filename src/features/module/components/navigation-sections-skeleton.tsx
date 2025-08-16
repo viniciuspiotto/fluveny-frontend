a8a90d500fe780f10c4f2ceb6483f8c9a389a9d1
viniciuspiotto <viniciuspiotto.dev@gmail.com>
2025-08-16 20:20:20 -0300

    styles: improve skeleton section buttons navigation

--- src/features/module/components/navigation-sections-skeleton.tsx
+++ src/features/module/components/navigation-sections-skeleton.tsx
@@ -1,11 +1,11 @@
 export const NavigationSectionsSkeleton = () => {
   return (
     <footer className="bg-primary fixed bottom-0 left-0 w-full overflow-x-auto">
-      <div className="flex w-max items-center justify-center gap-4 px-8 py-4 lg:w-full">
+      <div className="flex w-full items-center justify-center gap-3 px-4 py-4 lg:w-full lg:gap-6 lg:px-8">
         {Array.from({ length: 8 }).map((_, i) => (
           <div
             key={i}
-            className="h-12 w-10 animate-pulse rounded-md bg-zinc-300/20 lg:w-50"
+            className="size-12 animate-pulse rounded-md bg-zinc-300/20 lg:w-50"
           />
         ))}
       </div>
